b784eb514dc99687a40d4416f9117fe1d3e8a90a
doghoney <andrew1044@naver.com>
2022-08-23 21:31:28 +0900

    STT 확인

--- app/src/main/java/com/example/kiosk/V_main.java
+++ app/src/main/java/com/example/kiosk/V_main.java
@@ -1,18 +1,32 @@
 package com.example.kiosk;
 
+import android.Manifest;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
+import android.widget.Toast;
+
 import static android.speech.tts.TextToSpeech.ERROR;
+
+import androidx.core.app.ActivityCompat;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class V_main extends Service {
 
     private TextToSpeech tts;
+    final int PERMISSION = 1;
+    Intent sttIntent;
+    SpeechRecognizer mRecognizer;
 
     public V_main() {
     }
@@ -28,6 +42,27 @@ public class V_main extends Service {
                 }
             }
         });
+
+        /*if(Build.VERSION.SDK_INT >= 23){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET,
+            Manifest.permission.RECORD_AUDIO}, PERMISSION);
+        }*/
+
+        sttIntent=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        sttIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,getPackageName());
+        sttIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"ko-KR");
+
+        /*sttBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mRecognizer= SpeechRecognizer.createSpeechRecognizer(V_main.this);
+                mRecognizer.setRecognitionListener(listener);
+                mRecognizer.startListening(sttIntent);
+            }
+        });*/
+
+        mRecognizer= SpeechRecognizer.createSpeechRecognizer(V_main.this);
+        mRecognizer.startListening(sttIntent);
     }
 
     @Override
@@ -55,4 +90,87 @@ public class V_main extends Service {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
     }
+
+    private RecognitionListener listener = new RecognitionListener() {
+        @Override
+        public void onPartialResults(Bundle bundle) {
+
+        }
+
+        @Override
+        public void onEvent(int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onReadyForSpeech(Bundle params) {
+            //Toast.makeText(getApplicationContext(),"음성인식을 시작합니다.",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {
+        }
+
+        @Override
+        public void onRmsChanged(float rmsdB) {
+        }
+
+        @Override
+        public void onBufferReceived(byte[] buffer) {
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+        }
+
+        @Override
+        public void onError(int error) {
+            String message;
+
+            switch (error) {
+                case SpeechRecognizer.ERROR_AUDIO:
+                    message = "오디오 에러";
+                    break;
+                case SpeechRecognizer.ERROR_CLIENT:
+                    message = "클라이언트 에러";
+                    break;
+                case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                    message = "퍼미션 없음";
+                    break;
+                case SpeechRecognizer.ERROR_NETWORK:
+                    message = "네트워크 에러";
+                    break;
+                case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                    message = "네트웍 타임아웃";
+                    break;
+                case SpeechRecognizer.ERROR_NO_MATCH:
+                    message = "찾을 수 없음";
+                    break;
+                case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                    message = "RECOGNIZER가 바쁨";
+                    break;
+                case SpeechRecognizer.ERROR_SERVER:
+                    message = "서버가 이상함";
+                    break;
+                case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                    message = "말하는 시간초과";
+                    break;
+                default:
+                    message = "알 수 없는 오류임";
+                    break;
+            }
+
+            //Toast.makeText(getApplicationContext(), "에러가 발생하였습니다. : " + message, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onResults(Bundle results) {
+            ArrayList<String> matches =
+                    results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+            tts.speak(matches.toString(), TextToSpeech.QUEUE_FLUSH, null);
+            //FuncVoiceOrderCheck(rs[0]);
+            mRecognizer.startListening(sttIntent);
+        }
+    };
 }
