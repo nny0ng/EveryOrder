835bec2b5a0ad02b6a938aac67f987dbb4e66e2f
nnyong <68502088+nny0ng@users.noreply.github.com>
2022-08-23 20:46:51 +0900

    TTS

--- app/src/main/java/com/example/kiosk/V_main.java
+++ app/src/main/java/com/example/kiosk/V_main.java
@@ -3,19 +3,46 @@ package com.example.kiosk;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.speech.tts.TextToSpeech;
+import static android.speech.tts.TextToSpeech.ERROR;
+
+import org.w3c.dom.Text;
+
+import java.util.Locale;
 
 public class V_main extends Service {
+
+    private TextToSpeech tts;
+
     public V_main() {
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener(){
+            @Override
+            public void onInit(int status){
+                if(status != ERROR){
+                    tts.setLanguage(Locale.KOREAN);
+                }
+            }
+        });
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        if(intent == null){
+            return Service.START_STICKY;
+        }else{
+            processCommand(intent, flags, startId);
+        }
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void processCommand(Intent intent, int flags, int startId){
+        //tts로 설명문구 : label1
+        tts.speak("메뉴를 듣고싶으시면 메뉴, 주문을 하려면 주문, 직원 호출을 원하시면 호출 이라고 말해주세요", TextToSpeech.QUEUE_FLUSH, null);
     }
 
     @Override
