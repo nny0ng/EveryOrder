d1b9939cef1ae4e232fe3d1dafead207e77e64cb
LeeMinJi <julia7450@naver.com>
2022-08-24 16:26:13 +0900

    호출

--- app/src/main/java/com/example/kiosk/MainActivity.java
+++ app/src/main/java/com/example/kiosk/MainActivity.java
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import java.util.Arrays;
 import java.util.List;
@@ -32,6 +33,18 @@ public class MainActivity extends AppCompatActivity {
 
         fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.layout_frame, fragment_menu).commitAllowingStateLoss();
+
+        // 호출 버튼 클릭시 화면전환
+        Button button_call = (Button) findViewById(R.id.btn_call);
+        button_call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), PopupCall.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 
     public void clickHandler(View view)
